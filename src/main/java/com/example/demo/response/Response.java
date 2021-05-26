4c439b85c13dea3576a74aa8859bee0bb29bf24f
Itachi <annguyenvan.2k@gmail.com>
2021-05-26 15:05:23 +0700

    add Response object

---
+++ src/main/java/com/example/demo/response/Response.java
@@ -0,0 +1,18 @@
+package com.example.demo.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Response {
+    private String message;
+    private Integer status;
+    private List<Object> data;
+}
